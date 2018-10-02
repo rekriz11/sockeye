659936b4b10d5ae1a32d5756bdc68e8e7c3a589f
Tobias Domhan <domhant@amazon.de>
2018-10-02 16:26:52 +0200

    Revert "slightly cleaner"
    This reverts commit a341e0dac0a6c5691b2b68b7e8e54785531c6ea6.

--- sockeye/train.py
+++ sockeye/train.py
@@ -11,14 +11,21 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-# TODO: check how this behaves on windows
-from multiprocessing import forkserver
-from multiprocessing import semaphore_tracker
+import multiprocessing as mp
+import os
+import time
+print("PID: ", os.getpid())
+
+
+def dummy_function_to_start_semaphore_tracker():
+    print('Semphore tracker and forkserver started.')
 
 
 if __name__ == "__main__":
-    semaphore_tracker.ensure_running()
-    forkserver.ensure_running()
+    mp.set_start_method("forkserver")
+    p = mp.Process(target=dummy_function_to_start_semaphore_tracker)
+    p.start()
+    p.join()
 
 
 """
